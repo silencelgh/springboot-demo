ca1b53919a87eb6311ed01b4aa6607d741207e74
liuguohui <liuguohui0606@163.com>
2018-10-19 10:49:56 +0800

    修改文件包名

--- src/main/java/com/silence/springboot/SpringbootDemoApplication.java
+++ src/main/java/com/silence/springboot/SpringbootDemoApplication.java
@@ -1,4 +1,4 @@
-package com.silence.springbootdemo;
+package com.silence.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
